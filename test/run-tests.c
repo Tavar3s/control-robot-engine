da8355a6b8963b0b7ef42b5816c67e03535bcc9b
Gustavo Tavares <127619878+Tavar3s@users.noreply.github.com>
2025-07-17 17:04:45 -0300

    Update files
    The TTF_OpenFont didn't work (Library is not initiated) and I can't find a solution. Try to find freetype.dll file but still not working

--- test/run-tests.c
+++ test/run-tests.c
@@ -3,16 +3,21 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3_ttf/SDL_ttf.h>
 #include "../include/cre_menu-main.h"
+#include <stdio.h>
+
+#define FONT_PATH "C:/Users/Usuario/c-and-cpp-college/control-robot-engine/font/courierprimeregular.ttf"
 
 /*gcc .\test\run-tests.c .\source\cre_menu-main.c -o .\output\teste.exe -I "C:\SDL\x86_64-w64-mingw32\include" -I "C:\SDL_ttf\x86_64-w64-mingw32\include" -L "C:\SDL\x86_64-w64-mingw32\lib" -L "C:\SDL_ttf\x86_64-w64-mingw32\lib" -lSDL3 -lSDL3_ttf*/
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+static TTF_Font *font_app;
+static SDL_Texture *texture;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     SDL_SetAppMetadata("Run test app", "1.0", "com.run.test-app");
 
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
+    if (!SDL_Init(SDL_INIT_VIDEO) && !TTF_Init()) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
@@ -22,11 +27,31 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
         return SDL_APP_FAILURE;
     }
 
+    font_app = TTF_OpenFont("font/courierprimeregular.ttf", 36);
+    if (!font_app) {
+        SDL_Log("Couldn't load font: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_Surface *surface = TTF_RenderText_Blended(font_app, "Hello, World!", 0, (SDL_Color){255, 255, 255, 255});
+    if (!surface) {
+        SDL_Log("Couldn't render text: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_DestroySurface(surface);
+    surface = NULL;
+
+    if (!texture) {
+        SDL_Log("Couldn't create texture from surface: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-{
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
     }
@@ -54,6 +79,8 @@ SDL_AppResult SDL_AppIterate(void *appstate)
         draw_hud(renderer, 640, 480, 20, 5, 2); // Draw the HUD/menu overlay
     }
 
+    SDL_RenderTexture(renderer, texture, NULL, NULL);
+
     SDL_RenderPresent(renderer);
 
     return SDL_APP_CONTINUE;  /* carry on with the program! */
@@ -62,5 +89,13 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 /* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-    
+    if (font_app) {
+        TTF_CloseFont(font_app);
+        font_app = NULL;
+    }
+
+    if (texture) {
+        SDL_DestroyTexture(texture);
+        texture = NULL;
+    }
 }
