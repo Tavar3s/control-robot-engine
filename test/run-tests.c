34ff0d87305a501853743d5f2f9588feb7463e8b
Gustavo Tavares <127619878+Tavar3s@users.noreply.github.com>
2025-07-18 17:29:11 -0300

    Added support files and Using, now,  MSYS2 and Cmake to include external library

--- test/run-tests.c
+++ test/run-tests.c
@@ -1,7 +1,7 @@
 #define SDL_MAIN_USE_CALLBACKS 1  
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-#include <SDL3_ttf/SDL_ttf.h>
+#include "SDL3/SDL.h"
+#include "SDL3/SDL_main.h"
+#include "SDL3_ttf/SDL_ttf.h"
 #include "../include/cre_menu-main.h"
 #include <stdio.h>
 
@@ -27,7 +27,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
         return SDL_APP_FAILURE;
     }
 
-    font_app = TTF_OpenFont("font/courierprimeregular.ttf", 36);
+    font_app = TTF_OpenFont("../font/courierprimeregular.ttf", 36);
     if (!font_app) {
         SDL_Log("Couldn't load font: %s", SDL_GetError());
         return SDL_APP_FAILURE;
@@ -62,7 +62,12 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
             handle_menu_event(); // Toggle rectangle visibility when E is released
         }
         if (sym == SDLK_ESCAPE) {
-            return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+            if(get_menu_visibility()) {
+                handle_menu_event(); // Hide the rectangle when Escape is pressed
+            } else {
+                SDL_Quit();
+                return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+            }
         }
     }
 
